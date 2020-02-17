f2c1bb54ede9b15a3d9290891c38874b78c88f69
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-02-17 12:17:16 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # variables supplied from terraform.tfvars
 
 provider "ibm" {
-  iaas_classic_username  = var.ibm_sl_username
-  iaas_classic_api_key   = var.ibmcloud_iaas_api_key
-  ibmcloud_api_key       = var.ibmcloud_api_key
+  iaas_classic_username  = "${var.ibm_sl_username}"
+  iaas_classic_api_key   = "${var.ibm_sl_api_key}"
+  ibmcloud_api_key       = "${var.ibm_bmx_api_key}"
 }
