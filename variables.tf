9b8d5f7525f9b366aee8fc23363e4d0def0cf20a
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-02-17 11:11:59 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,49 @@
+#---------------------------------------------------------
+# MODIFY VARIABLES AS NEEDED
+#---------------------------------------------------------
+variable "iaas_username" {
+          description = "add your IBM Cloud username"
+          default = ""
+          }
+
+variable "ibmcloud_iaas_api_key" {
+          description = "add your IBM Cloud IaaS API Key
+	  default = ""
+          }
+
+variable "ibmcloud_api_key" {
+          description = "add your IBM Cloud API Key
+          default = ""
+          }
+
+#---------------------------------------------------------
+## DEFINE sshkey to be used for compute instances
+#---------------------------------------------------------
+#variable "ssh_keyname" {
+#          description = "add your SSH Key"
+#          default = "" 
+#         }
+
+#---------------------------------------------------------
+## DEFINE Ressource Group (IAM)
+#---------------------------------------------------------
+variable "resource_group" {
+  description = "The Ressource Roup must be defined in IBM Cloud IAM"
+  default = "default"
+}
+
+
+#---------------------------------------------------------
+
+
+variable "server-name" {
+  default = "servername"
+}
+
+variable "server-count" {
+  default = 1
+}
+
+variable "ibmcloud_timeout" {
+   default = 300
+}
