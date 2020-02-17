7b1fa196633b26d8dc8143b3f8f4204e7ce4b3c6
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-02-17 12:36:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,3 +39,8 @@ variable "server-count" {
 variable "ibmcloud_timeout" {
    default = 300
 }
+
+variable "ibmcloud_OS-system" {
+   description = "you can deploy different OS system on VSI like WIN / Linux. Use the specific image reference code like WIN_2012-STD-R2_64 or CENTOS_LATEST_64  . use CLI comand: $ibmcloud sl vs options"
+   default = "CENTOS_LATEST_64"
+}
