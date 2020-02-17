f8980353be8a5c9de75b9aadfa08e2afdd89ed64
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-02-17 12:30:46 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "iaas_classic_username" {}
 
 variable "iaas_classic_api_keyy" {}
 
-variable "ibmcloud_api_key" {}
+# variable "ibmcloud_api_key" {}
 
 
 #---------------------------------------------------------
