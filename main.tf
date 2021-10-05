3749b4a9223aaf9c33917a94071b9b933f839e0b
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-05 17:25:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 #---------------------------------------------------------
 # Create instance VSI
 #---------------------------------------------------------
-resource "ibm_compute_vm_instance" "terraform-bulk-vms" {
+resource "ibm_compute_vm_instance" "veeam-os360-server" {
   bulk_vms {
     hostname = "${var.server-name}-vm1"
-    domain = "veeam-os360.com"
+    domain = "${var.domain-name}"
   }
   os_reference_code    = "${var.ibmcloud_OS-system}"
   datacenter           = "${var.ibmcloud_datacenter}"
