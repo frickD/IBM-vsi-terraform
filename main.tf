27f46503519bd09ad3e6f8682b1fd04d469f543e
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-05 17:12:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
+#---------------------------------------------------------
+# Create instance VSI
+#---------------------------------------------------------
 resource "ibm_compute_vm_instance" "terraform-bulk-vms" {
   bulk_vms {
     hostname = "${var.server-name}-vm1"
-    domain = "poc.com"
-  }
-  bulk_vms {
-    hostname = "${var.server-name}-vm2"
-    domain = "poc.com"
+    domain = "veeam-os360.com"
   }
   os_reference_code    = "${var.ibmcloud_OS-system}"
   datacenter           = "${var.ibmcloud_datacenter}"
   network_speed        = 1000
   hourly_billing       = true
   private_network_only = true
-  cores                = 1
-  memory               = 4096
+  cores                = 4
+  memory               = 8192
   disks                = [100]
   local_disk           = false
 }
