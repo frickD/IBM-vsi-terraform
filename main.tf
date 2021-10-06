82db832f607c55593ddf1c5b2968e26f0bf7f35f
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-06 08:53:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #---------------------------------------------------------
 # public SSH key
 #---------------------------------------------------------
-resource "ibm_compute_ssh_key" "ssh_key_gip" {
+resource "ibm_compute_ssh_key" "ssh_key_vsi" {
   label      = var.ssh_label
   public_key = var.ssh_public_key
 }
@@ -18,9 +18,15 @@ resource "ibm_compute_vm_instance" "veeam-os360-server" {
   datacenter           = "${var.ibmcloud_datacenter}"
   network_speed        = 1000
   hourly_billing       = true
-  private_network_only = true
+  private_network_only = false
   cores                = 4
   memory               = 8192
   disks                = [100]
   local_disk           = false
+  ssh_key_ids = [
+    ibm_compute_ssh_key.ssh_key_vsi.id,
+  ]
+  #provisioner "remote-exec" {
+  #  script = "remote_sw_deploy.sh"
+  #}
 }
