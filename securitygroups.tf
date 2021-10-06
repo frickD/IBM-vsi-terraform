2558d4c286a31cd27317955b4c46a51d248a7e4c
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-06 15:16:43 +0200

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -6,15 +6,6 @@ resource "ibm_security_group" "secgroup1" {
     description = "allow my traffic for Veeam Server"
 }
 
-resource "ibm_security_group_rule" "allow_port_22" {
-    direction = "ingress"
-    ether_type = "IPv4"
-    port_range_min = 22
-    port_range_max = 22
-    protocol = "tcp"
-    security_group_id = ibm_security_group.secgroup1.id
-}
-
 resource "ibm_security_group_rule" "allow_rdh_tcp" {
     direction = "ingress"
     ether_type = "IPv4"
