5b84038d9b3b5317692007695b5fe72a226e0dd7
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-06 10:32:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,8 @@ resource "ibm_compute_ssh_key" "ssh_key_vsi" {
 # Create instance VSI
 #---------------------------------------------------------
 resource "ibm_compute_vm_instance" "veeam-os360-server" {
-  bulk_vms {
-    hostname = "${var.server-name}-vm1"
-    domain = "${var.domain-name}"
-  }
+  hostname = "${var.server-name}"
+  domain = "${var.domain-name}"
   os_reference_code    = "${var.ibmcloud_OS-system}"
   datacenter           = "${var.ibmcloud_datacenter}"
   network_speed        = 1000
