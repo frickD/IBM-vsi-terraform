f3cc223f1529b2e7b3eb107b79c3826c3ffb4038
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-07 14:17:33 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@
 ## Log output public IP
 #---------------------------------------------------------
 
-output "veeam-os360-ip"{
-  value = "http://${ibm_network_public_ip.veeam-os360-ip.ip_address}"
+output "veeam-os365-ip"{
+  value = "http://${ibm_network_public_ip.veeam-os365-ip.ip_address}"
 }
