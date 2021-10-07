b617641ee428d7349c9dd9df05a4bf2558dd0954
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-07 09:29:56 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,6 +25,10 @@ variable "ibmcloud_datacenter" {
    default = "fra02"
   }
 
+variable "ibmcloud_pulic_vlan_id" {
+   description = "you can add the existing public VLAN-ID from traget data center"
+  }
+
 variable "ibmcloud_OS-system" {
    description = "you can deploy different OS system on VSI like WIN / Linux. Use the specific image reference code like WIN_2012-STD-R2_64 or CENTOS_LATEST_64  . use CLI comand: $ibmcloud sl vs options"
    default = "WIN_LATEST_64"
