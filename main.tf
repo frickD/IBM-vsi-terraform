69bc3c8aea361df2176070c83897fdfb84cb5289
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-07 16:05:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,47 @@
 #---------------------------------------------------------
+# Create Security Group & Rules
+#---------------------------------------------------------
+resource "ibm_security_group" "secgroup1" {
+    name = "secgroup1"
+    description = "allow my traffic for Veeam Server"
+}
+
+resource "ibm_security_group_rule" "allow_rdh_tcp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 3389
+    port_range_max = 3389
+    protocol = "tcp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_rdh_udp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 3389
+    port_range_max = 3389
+    protocol = "udp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_in_icmp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    protocol = "icmp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_out_all" {
+    direction = "egress"
+    ether_type = "IPv4"
+    port_range_min = 1
+    port_range_max = 65000
+    protocol = "tcp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+
+#---------------------------------------------------------
 # Create instance VSI
 #---------------------------------------------------------
 resource "ibm_compute_vm_instance" "veeam-os365-server" {
@@ -15,7 +58,7 @@ resource "ibm_compute_vm_instance" "veeam-os365-server" {
   local_disk           = false
   public_vlan_id       = "${var.ibmcloud_pulic_vlan_id}"
   private_vlan_id      = "${var.ibmcloud_private_vlan_id}"
-  public_security_group_ids    = "${ibm_security_group.secgroup1.id}"
+  public_security_group_ids    = [ibm_security_group.secgroup1.id]
   
   #provisioner "remote-exec" {
   #  script = "remote_sw_deploy.sh"
