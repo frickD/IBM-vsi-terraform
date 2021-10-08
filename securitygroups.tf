6ce1dae79ed86d355a14eaae785f5ee378f0a0e3
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-08 09:07:03 +0200

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,1 +1,43 @@
+#---------------------------------------------------------
+# Create Security Group & Rules
+#---------------------------------------------------------
+resource "ibm_security_group" "secgroup1" {
+    name = "secgroup1"
+    description = "allow my traffic for Veeam Server"
+}
+
+resource "ibm_security_group_rule" "allow_rdh_tcp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 3389
+    port_range_max = 3389
+    protocol = "tcp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_rdh_udp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    port_range_min = 3389
+    port_range_max = 3389
+    protocol = "udp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_in_icmp" {
+    direction = "ingress"
+    ether_type = "IPv4"
+    protocol = "icmp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
+resource "ibm_security_group_rule" "allow_out_all" {
+    direction = "egress"
+    ether_type = "IPv4"
+    port_range_min = 1
+    port_range_max = 65000
+    protocol = "tcp"
+    security_group_id = ibm_security_group.secgroup1.id
+}
+
 
