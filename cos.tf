68b8b4b630f5d7ec235a91c7244abed0e267dd61
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-08 09:20:06 +0200

    Update cos.tf

--- cos.tf
+++ cos.tf
@@ -3,7 +3,8 @@
 #-----------------------------------------------------
 resource "ibm_resource_instance" "veeam_cos_instance" {
   name              = "veeam-cos-instance"
-  resource_group_id = "${var.resource_group}"
+# resource_group_id = "${var.resource_group}"
+  resource_group_id = data.ibm_resource_group.resource_group.id
   service           = "veeam-OS365-Bucket"
   region_location   = "eu-de"
   storage_class     = "smart"
