0ec82cfe2e0e34f72ac56f5c6ce806de8089b5fc
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-08 09:27:50 +0200

    Update cos.tf

--- cos.tf
+++ cos.tf
@@ -5,13 +5,13 @@
 #---------------------------------------------------------
 ## DEFINE Ressource Group veeam-os365  or using default
 #---------------------------------------------------------
-#variable "resource_group" {
+#variable "resourcegroup" {
 #  description = "The Ressource Roup must be defined in IBM Cloud IAM"
 #  default = "default"
 #}
 
 # Create new resource group
-resource "ibm_resource_group" "resource_group" {
+resource "ibm_resource_group" "resourcegroup" {
   name     = "veeam-os365"
 }
 
@@ -20,8 +20,8 @@ resource "ibm_resource_group" "resource_group" {
 
 resource "ibm_resource_instance" "veeam_cos_instance" {
   name              = "veeam-cos-instance"
-# resource_group_id = "${var.resource_group}"
-  resource_group_id = data.ibm_resource_group.resource_group.id
+# resource_group_id = "${var.resourcegroup}"
+  resource_group_id = data.ibm_resource_group.resourcegroup.id
   service           = "veeam-OS365-Bucket"
   region_location   = "eu-de"
   storage_class     = "smart"
