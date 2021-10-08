412f33acfe32a5fcadc36a37c16369ad5b14d01d
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-08 09:03:12 +0200

    Update cos.tf

--- cos.tf
+++ cos.tf
@@ -1,1 +1,13 @@
+#-----------------------------------------------------
+# Create COS Instance for Veeam OS365 backup
+#-----------------------------------------------------
+resource "ibm_resource_instance" "veeam_cos_instance" {
+  name              = "veeam-cos-instance"
+  resource_group_id = "${var.resource_group}"
+  service           = "veeam-OS365-Bucket"
+  region_location   = "eu-de"
+  storage_class     = "smart"
+  plan              = "standard"
+  
+}
 
