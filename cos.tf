1274e223b4a04268324ec249192b9995ff2ab787
Joachim Fischer <joachim.fischer@de.ibm.com>
2021-10-08 09:24:23 +0200

    Update cos.tf

--- cos.tf
+++ cos.tf
@@ -1,6 +1,23 @@
 #-----------------------------------------------------
 # Create COS Instance for Veeam OS365 backup
 #-----------------------------------------------------
+
+#---------------------------------------------------------
+## DEFINE Ressource Group veeam-os365  or using default
+#---------------------------------------------------------
+#variable "resource_group" {
+#  description = "The Ressource Roup must be defined in IBM Cloud IAM"
+#  default = "default"
+#}
+
+# Create new resource group
+resource "ibm_resource_group" "resource_group" {
+  name     = "veeam-os365"
+}
+
+#---------------------------------------------------------
+
+
 resource "ibm_resource_instance" "veeam_cos_instance" {
   name              = "veeam-cos-instance"
 # resource_group_id = "${var.resource_group}"
